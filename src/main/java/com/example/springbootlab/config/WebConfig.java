75533fecd8503182c150e466c830285e191aa256
Vance <vance.tw@gmail.com>
2025-12-01 22:31:23 +0800

    fix: 為 addResourceHandlers 方法的參數添加 @NonNull 註解以強化參數檢查

--- src/main/java/com/example/springbootlab/config/WebConfig.java
+++ src/main/java/com/example/springbootlab/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.example.springbootlab.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,7 +19,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    public void addResourceHandlers(@NonNull ResourceHandlerRegistry registry) {
         // 將所有請求對應到專案根目錄下的 docs 資料夾
         registry.addResourceHandler("/**")
                 .addResourceLocations("file:docs/");
